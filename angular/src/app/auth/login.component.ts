614517892d9e9ef5daa1956ee7bf85e743d5fdc6
Khova Krishna Pilato <krishnak.pilato@gmail.com>
2022-10-22 11:25:55 +0200

    bug fixing

--- angular/src/app/auth/login.component.ts
+++ angular/src/app/auth/login.component.ts
@@ -6,7 +6,8 @@ import { LoginRequest } from './loginrequest';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginRequest: LoginRequest;
