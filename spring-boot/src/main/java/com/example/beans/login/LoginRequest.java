c4264d267f5d9c2e735af8e51e5ce4deb7b599e0
Khova Krishna Pilato <krishnak.pilato@gmail.com>
2025-01-01 10:01:25 +0100

    clean

--- spring-boot/src/main/java/com/example/beans/login/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.beans.login;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-	private String username, password;
-}
--- spring-boot/src/main/java/com/example/enums/ApplicationRole.java
+++
@@ -1,5 +0,0 @@
-package com.example.enums;
-
-public enum ApplicationRole {
-	USER, MANAGER, ADMINISTRATOR
-}
